a89be8a82ce5ea9cd5606622364daf713d31b644
Hiroyuki Ota <buzzybuzz.design@gmail.com>
2024-03-24 16:59:16 +0900

    1.1：コントローラー

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import {
+  CreateCatDto,
+  UpdateCatDto,
+  ListAllEntities,
+} from './dto/create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(@Res() res: Response, @Body() createCatDto: CreateCatDto) {
+    return res.status(HttpStatus.CREATED).send(createCatDto);
+  }
+
+  @Get()
+  findAll(
+    @Res({ passthrough: true }) res: Response,
+    @Query() query: ListAllEntities,
+  ) {
+    res.status(HttpStatus.OK);
+    return `This action returns all cats (limit: ${query.limit} items)`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
